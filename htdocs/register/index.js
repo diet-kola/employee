e7873c3a3fcb1a47fb419b40345f421edccab016
diet.kola <rclibao965@gmail.com>
2025-10-12 13:14:24 +0800

    delete index.js from register

--- htdocs/register/index.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const btn = document.getElementById('signIn');
-//     btn?.addEventListener('click', e => {
-//         window.location.href = '../registerSuccessful/index.php';
-//     });
-// });
