e69011381efc9b4eee928a0a30a8434a4f5b7b86
diet.kola <rclibao965@gmail.com>
2025-11-10 17:53:55 +0800

    made admin an employee, did some  restructuring to accomodate changes

--- src/functions/login-function.php
+++ src/functions/login-function.php
@@ -5,25 +5,33 @@ session_start();
 $conn = connectDB();
 $error = "";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+    {
+    //kuha inputs
     $email = trim($_POST["email"]);
     $password = $_POST['password'];
     
     // check kung empty yung email at password
-    if (empty($email))
-    { $error = "Email is required";} 
-    elseif (empty($password)) 
-    { $error = "Password is required";}
+    if (empty($email)) { $error = "Email is required";} 
+    elseif (empty($password)) { $error = "Password is required";}
     else 
     {
-        $check = $conn->prepare("SELECT * FROM admin_user WHERE email = ?");
+        $check = $conn->prepare("SELECT e.first_name, e.last_name, a.password 
+                                    FROM admin_user a 
+                                 JOIN employees e on e.employee_id = a.employee_id
+                                    WHERE email = ? AND e.position_id = 9
+                                    ");
         $check->execute([$email]);
         $check = $check->fetch();
 
-        if ($check && password_verify($password, $check['password'])) {
+        if ($check && password_verify($password, $check['password'])) 
+        {
+            $_SESSION['admin_name'] = $check['first_name'] . ' ' . $check['last_name'];
             header('Location: ../mainPage');
             exit;
-        } else { 
+        } 
+        else 
+        { 
             $error = "Invalid email or password"; 
         }
     }
