08c94cbb73f4cb3ecab9d7ff73c1a98f7b9e6aa6
diet.kola <rclibao965@gmail.com>
2025-11-13 21:49:27 +0800

    updateEmployee function and csss for updateEmployee

--- src/functions/CRUD/updateEmployee.php
+++ src/functions/CRUD/updateEmployee.php
@@ -1,5 +1,67 @@
 <?php
-require_once __DIR__ . '/../../config.php/databse.php';
+require_once __DIR__ . '/../../config/database.php';
+session_start();
 
 $conn = connectDB();
+$employeeId = $_POST['updateId'];
 
+// get employee
+$getEmployee = $conn->prepare("SELECT * FROM employees WHERE employee_id = ?");
+$getEmployee->execute([$employeeId]);
+$employee = $getEmployee->fetch();
+
+// get admin if employee is admin
+$getAdmin = $conn->prepare("SELECT * FROM admin_user WHERE employee_id = ?");
+$getAdmin->execute([$employeeId]);
+$admin = $getAdmin->fetch();
+
+//get all positions
+$positions = $conn->query("SELECT * FROM positions ORDER BY position_id");
+$positions = $positions->fetchAll();
+
+$employeeName = $employee['first_name'] . " " . $employee['last_name'];
+
+if (!empty($admin['admin_id']) && $admin['admin_id'] == $_SESSION['admin_id']) 
+{ 
+  $_SESSION['message'] = $employeeName . " is currently logged in and can't be updated"; 
+  header("Location: ../../mainPage"); // redirect back to main page
+  exit;
+}
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['first_name'])) 
+{
+  $firstName = trim($_POST['first_name']);
+  $lastName = trim($_POST['last_name']);
+  $email = trim($_POST['email']);
+  $contactNo = trim($_POST['contact_no']);
+  $positionId = $_POST['position_id'];
+
+  if (empty($firstName)) { $error = "First name is required.";}
+  elseif (empty($lastName)) { $error = "Last name is required.";}
+  elseif (empty($email)) { $error = "Email is required.";}
+  elseif (empty($contactNo)) { $error = "Contact number is required.";}
+  elseif (empty($positionId)) { $error = "Position is required."; }
+  else
+  {
+    if (!empty($employee['position_id']) && $employee['position_id'] == 9) 
+    {
+      $removeAdmin = $conn->prepare("DELETE FROM admin_user WHERE employee_id = ?");
+      $removeAdmin->execute([$employeeId]); 
+    }
+
+    if ($positionId == 9 && empty($admin)) {
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $insertAdmin = $conn->prepare("INSERT INTO admin_user (employee_id, password) VALUES (?, ?)");
+            $insertAdmin->execute([$employeeId, $hashedPassword]);
+    }
+    
+    $update = $conn->prepare("UPDATE employees
+                              SET first_name = ?, last_name = ?, email = ?, contact_no = ?, position_id = ?
+                              WHERE employee_id = ?
+                            ");
+    $update->execute([$firstName, $lastName, $email, $contactNo, $positionId, $employeeId]);
+
+    $_SESSION['message'] = $employeeName . "'s information has been updated";
+    header("Location: ../../mainPage"); // redirect back to main page
+    exit;
+  }
+}
