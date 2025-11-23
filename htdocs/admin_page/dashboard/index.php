bf605125a470a3db5ecae098e9cb486643e7f8cb
diet.kola <rclibao965@gmail.com>
2025-11-23 20:39:02 +0800

    dashboard

---
+++ htdocs/admin_page/dashboard/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Sidebar -->
+    <?php include '../../_reusables/sidebar.php'; ?>
+
+    <!-- Header -->
+    <?php include '../../_reusables/header.php'; ?>
+</body>
+</html>
