a868b618f4feba2e3a0ffddd1210d2c0c9deb55a
diet.kola <rclibao965@gmail.com>
2025-11-23 21:12:30 +0800

    added proper links

--- htdocs/_reusables/sidebar.php
+++ htdocs/_reusables/sidebar.php
@@ -1,8 +1,8 @@
 <div id="sidebar" class="sidebar">
     <a href="javascript:" class="closebtn" onclick="closeNav()">&times;</a>
-    <a href="dashboard.php">Dashboard</a>
-    <a href="employees.php">Employees</a>
-    <a href="positions.php">Positions</a>
-    <a href="profile.php">Profile</a>
+    <a href="../dashboard">Dashboard</a>
+    <a href="../employees">Employees</a>
+    <a href="../positions">Positions</a>
+    <a href="../profile">Profile</a>
     <a href="logout.php">Logout</a>
 </div>
