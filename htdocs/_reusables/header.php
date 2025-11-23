4743b8622b92f176911ddd3d4afe6b8fba9f7aa3
diet.kola <rclibao965@gmail.com>
2025-11-23 20:38:23 +0800

    separated to be reused

---
+++ htdocs/_reusables/header.php
@@ -0,0 +1,11 @@
+<div class = "header-container">
+    <span class="hamburger-bar" onclick="openNav()">
+        <img src="../../_resources/borgir-menu.png" width="40px">
+    </span>
+
+    <div class="header" id="header">
+        <h2>Hotel Employee Tracker</h2>
+        <p>Logged in as: <?= $_SESSION['admin_name'] ?></p>
+    </div>
+</div>
+
