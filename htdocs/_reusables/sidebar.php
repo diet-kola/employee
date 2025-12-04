a99b8404957e08048d2be0fc45cae584c1454c91
diet.kola <rclibao965@gmail.com>
2025-12-04 08:46:59 +0800

    changed redidrect to start from docroot

--- htdocs/_reusables/sidebar.php
+++ htdocs/_reusables/sidebar.php
@@ -1,8 +1,8 @@
 <div id="sidebar" class="sidebar">
     <a href="javascript:" class="closebtn" onclick="closeNav()">&times;</a>
-    <a href="../dashboard">Dashboard</a>
-    <a href="../employees">Employees</a>
-    <a href="../positions">Positions</a>
-    <a href="../profile">Profile</a>
-    <a href="logout.php">Logout</a>
+    <a href="/admin_page/dashboard">Dashboard</a>
+    <a href="/admin_page/employees">Employees</a>
+    <a href="/admin_page/schedules">Schedules</a>
+    <a href="/profile_page">Profile</a>
+    <a href="/logout">Logout</a>
 </div>
